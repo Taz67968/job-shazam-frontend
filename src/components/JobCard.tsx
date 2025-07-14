88ebb5d2f5946c512f9f72192130798d8a7f6ab2
Tazohcliff <tazohcliff@gmail.com>
2025-07-14 11:17:29 +0100

    feat: edit

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -17,13 +17,16 @@ interface JobCardProps {
 const JobCard: React.FC<JobCardProps> = ({ job }) => {
   return (
     <div className="job-card">
+      <div className="flex justify-between items-center">
       <h3 className="job-title">{job.title}</h3>
-      <p className="job-company">{job.company}</p>
-      <p className="job-location">{job.location}</p>
-      <p className="job-type">{job.type}</p>
+
       <a href={job.url} target="_blank" rel="noopener noreferrer" className="apply-button">
         Apply Now
       </a>
+      </div>
+      <p className="job-company">{job.company}</p>
+      <p className="job-location">{job.location}</p>
+      <p className="job-type">{job.type}</p>
       <Link href={`/jobsDetailspage/${job.id}`} className="view-job-link">
         View Job
       </Link>
